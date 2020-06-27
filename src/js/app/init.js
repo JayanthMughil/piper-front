9302425094e9b7ba8c9bd966d621fe64a0cc7eb6
JayanthMughil <abaianjayanthanan@gmail.com>
2020-06-27 09:00:38 +0530

    bootstrap included

--- src/js/app/init.js
+++ src/js/app/init.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import '../../css/index.css';
 
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import "../../css/index.css";
+
+class Init extends Component {
+
+  componentDidMount = () => {
+    fetch("http://localhost:8080/PiedPiper/rest/helloapi",{
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then((resp) => {
+      console.log(resp);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
+  render () {
+    return (
+        <div>
+          <nav className="navbar navbar-light navbar-expand-md">
+            <div className="container-fluid">Pied Piper</div>
+          </nav>
+        </div>
+    );
+  }
+
+}
 
 const initialize = () => {
 
     ReactDOM.render(
-        <div>
-          Hello react is working
-        </div>,
+        <Init />,
         document.getElementById('OuterFrame')
       );
 
